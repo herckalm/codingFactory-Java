90878220733bc6ab17ffb2c54693524fe590d849
Iraklis Kalamas <iraklisnd@gmail.com>
2025-10-22 21:24:04 +0300

    corrections - NumberToDate.java

--- src/gr/aueb/cf/cf9/ch2/NumberToDate.java
+++ src/gr/aueb/cf/cf9/ch2/NumberToDate.java
@@ -2,12 +2,24 @@ package gr.aueb.cf.cf9.ch2;
 
 import java.util.Scanner;
 
+/**
+ * The program converts 3 integers into a date format.
+ * For example, given the integers 7, 12, and 2025, the output should be 7/12/2025.
+ * The user is prompted to enter a valid day (1-31), a valid month (1-12),
+ * and a year (4 digits). The program checks if each input is within the limits shown above
+ * and if all inputs are valid it prints an appropriate message in the console.
+ * If any of the inputs (day or month) are invalid, the user is provided with a message
+ * to correct that invalid input. If the user inputs 0 at any time, the program exits.
+ *
+ * @author Ηρακλής Καλαμάς
+ */
+
 public class NumberToDate {
     public static void main(String[] args) {
 
         // Δήλωση και αρχικοποίηση μεταβλητών
         Scanner scanner = new Scanner(System.in);
-        int day = -1, month = -1, year = -1;
+        int day, month, year;
 
         // Continues until the user chooses to exit (by entering 0 for day)
         while (true) {
@@ -53,7 +65,7 @@ public class NumberToDate {
                 break;
             }
 
-            // Check if the outer loop needs to exit (only happens if day was 0)
+            // Check if the outer loop needs to exit (only happens if month was 0)
             if (month == 0) {
                 break;
             }
@@ -61,6 +73,7 @@ public class NumberToDate {
             // year input
             System.out.print("Εισάγετε έτος (π.χ. 2025): ");
             year = scanner.nextInt();
+            // Check if the outer loop needs to exit (only happens if year was 0)
             if (year == 0) {
                 break;
             }
