302f5bb8b31b2a0a333d9e9a0002802ef6f7835f
herckalm <iraklisnd@gmail.com>
2025-10-27 08:55:13 +0200

    chapter3 exercise 2 final

--- src/gr/aueb/cf/cf9/ch3/Menu.java
+++ src/gr/aueb/cf/cf9/ch3/Menu.java
@@ -2,17 +2,27 @@ package gr.aueb.cf.cf9.ch3;
 
 import java.util.Scanner;
 
+/**
+ * This program presents to the user a menu of five options to choose from.
+ * If a user selects an option between 1 and 4, an appropriate message is
+ * displayed in the console, and the user is presented with the same options
+ * again. If the user enters 5, the program exits, notifying the user of the
+ * action. In any other case, a message is displayed notifying that the choice
+ * was invalid, and the program continues as normal.
+ */
+
 public class Menu {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        while (true) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
             System.out.println("1. Εισαγωγή");
             System.out.println("2. Διαγραφή");
             System.out.println("3. Αναζήτηση");
             System.out.println("4. Ενημέρωση");
             System.out.println("5. Έξοδος");
             System.out.print("Επιλέξτε μία από τις πιο πάνω επιλογές: ");
-            int choice = input.nextInt();
+            choice = scanner.nextInt();
             if (choice == 1) {
                 System.out.println("Επιλέξατε Εισαγωγή");
             } else if (choice == 2) {
@@ -23,11 +33,12 @@ public class Menu {
                 System.out.println("Επιλέξατε Ενημέρωση");
             } else if (choice == 5) {
                 System.out.println("Επιλέξατε έξοδο. Το πρόγραμμα τερματίστηκε!");
-                break;
             } else {
                 System.out.println("Η επιλογή σας δεν ήταν έγκυρη.");
                 System.out.println("Επιλέξτε μία από τις πιο πάνω επιλογές:");
             }
-        }
+            System.out.println();
+        } while (choice != 5);
+        scanner.close();
     }
 }
