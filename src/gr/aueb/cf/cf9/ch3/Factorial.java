b1920f29de44fdb8f6ac653cec5aca7a25ad3651
herckalm <iraklisnd@gmail.com>
2025-11-05 16:22:28 +0200

    chapter 5

--- src/gr/aueb/cf/cf9/ch3/Factorial.java
+++ src/gr/aueb/cf/cf9/ch3/Factorial.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Pleas give a number: ");
+        System.out.print("Please give a number: ");
         int num = scanner.nextInt();
         int result = 1;
 
         while (num > 1) {
             result *= num;
-            num -= 1;
+            num--;
         }
         System.out.println(result);
     }
